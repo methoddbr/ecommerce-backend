129d6396a51ae4967d15704643455cedfead06ec
Leonardo Prestes <methoddbr@gmail.com>
2023-05-12 20:19:35 -0300

    feat: insert dtos in city, state and address

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -40,7 +40,13 @@ export class UserService {
       where: {
         id: userId,
       },
-      relations: ['addresses'],
+      relations: {
+        addresses: {
+          city: {
+            state: true,
+          },
+        },
+      },
     });
   }
 }
