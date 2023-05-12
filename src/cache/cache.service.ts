23f98d5d986eec19c42e97626fda19aeba146ce8
Leonardo Prestes <methoddbr@gmail.com>
2023-05-12 16:14:15 -0300

    feat: created cache module

---
+++ src/cache/cache.service.ts
@@ -0,0 +1,23 @@
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CacheService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+  async getCache<T>(
+    key: string,
+    functionRequest: () => Promise<T>,
+  ): Promise<T> {
+    const allData: T = await this.cacheManager.get(key);
+
+    if (allData) {
+      return allData;
+    }
+    const request: T = await functionRequest();
+
+    await this.cacheManager.set(key, request);
+
+    return request;
+  }
+}
